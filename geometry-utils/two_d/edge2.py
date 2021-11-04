a67c7a9f4c5fc61ffaee5222a287498966bbd030
Marvellous Akinyele <marvellous.akinyele@gmail.com>
2021-11-04 16:50:03 +0000

    add point_parametric and parametric_point

--- geometry-utils/two_d/edge2.py
+++ geometry-utils/two_d/edge2.py
@@ -14,28 +14,30 @@ class Edge2:
         self.radius = radius
         self.clockwise = clockwise
         self.large = large
-        self.arc_centre = (p1 + p2) * 0.5
+        self.arc_centre = self.get_arc_centre()
 
     def point_parametric(self, s):
-        if s == 0:
-            return self.p1
-        elif s == 1:
-            return self.p2
-        elif s == 0.5:
-            return self.arc_centre
+        if isinstance(s, float):
+            if self.p1 == self.p2:
+                return self.p1
 
+            tangent = self.get_tangent()  # vector
+            p1_p2_distance = self.p1.distance_to(self.p2)  # vector
+            vector = tangent * (s * p1_p2_distance)  # vector
+            return self.p1 + vector  # point
 
-'''
-Point Equality and Inequality Tests
-'''
+    def parametric_point(self, point):
+        if isinstance(point, Point2):
+            tangent = self.get_tangent()  # vector
+            point_p1_difference = (point - self.p1).to_vector()  # vector
+            distance = tangent.dot(point_p1_difference)
+            return distance / self.p1.distance_to(self.p2)
 
+    def get_arc_centre(self):
+        if self.radius == 0.0:
+            return (self.p1 + self.p2) * 0.5
 
-def test_vector_vector_equality(test_point_1, test_point_5):
-    assert test_point_1 == test_point_1
-    assert test_point_1 == test_point_5
-
-
-def test_vector_vector_inequality(test_point_1, test_point_2):
-    assert test_point_1 != test_point_2
-    assert not (test_point_1 == test_point_2)
-    assert not test_point_1 == 9.0
+    def get_tangent(self):
+        p1_vector = self.p1.to_vector()
+        p2_vector = self.p2.to_vector()
+        return (p2_vector - p1_vector).normalise()
