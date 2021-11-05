7127fd53643038bc51239e4f73be0c0dae05abd0
Marvellous Akinyele <marvellous.akinyele@gmail.com>
2021-11-05 15:41:54 +0000

    Update maths_utility.py

--- geometry-utils/maths_utility.py
+++ geometry-utils/maths_utility.py
@@ -1,1 +1,6 @@
-M_PI = 3.14159265358
+def m_pi():
+    return 3.14159265358
+
+
+def floats_are_close(a, b, rel_tol=1e-09, abs_tol=0.0):
+    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
