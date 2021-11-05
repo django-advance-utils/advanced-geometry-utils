18cb5b09b8bbe81e0c57cbe99b3f84ed8e7294ca
Marvellous Akinyele <marvellous.akinyele@gmail.com>
2021-11-05 15:42:12 +0000

    Update test_edge2.py

--- geometry-utils/tests/two_d/test_edge2.py
+++ geometry-utils/tests/two_d/test_edge2.py
@@ -3,8 +3,10 @@ import pytest
 from two_d.edge2 import Edge2
 from two_d.point2 import Point2
 from two_d.vector2 import Vector2
+from maths_utility import floats_are_close
 
 from math import sqrt
+
 
 @pytest.fixture()
 def test_edge2_1():
@@ -23,6 +25,11 @@ def test_edge2_2():
 def test_edge_point_parametric(test_edge2_2):
     assert test_edge2_2.point_parametric(0.0) == test_edge2_2.p1
     assert test_edge2_2.point_parametric(1.0) == test_edge2_2.p2
+
+
+def test_edge_parametric_point(test_edge2_2):
+    assert test_edge2_2.parametric_point(test_edge2_2.p1) == 0.0
+    assert floats_are_close(test_edge2_2.parametric_point(test_edge2_2.p2), 1.0)
 
 
 def test_get_tangent(test_edge2_2):
