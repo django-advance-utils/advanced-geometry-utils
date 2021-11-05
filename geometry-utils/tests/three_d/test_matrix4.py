4ad00e1e0b388ae050d57d6685f495e3cae8c2fa
Marvellous Akinyele <marvellous.akinyele@gmail.com>
2021-11-05 14:11:53 +0000

    Update test_matrix4

--- geometry-utils/tests/three_d/test_matrix4.py
+++ geometry-utils/tests/three_d/test_matrix4.py
@@ -5,19 +5,19 @@ from three_d.vector3 import Vector3
 
 
 @pytest.fixture
-def test_matrix_1():
+def test_matrix4_1():
     return Matrix4()
 
 
 @pytest.fixture()
-def test_matrix_2():
+def test_matrix4_2():
     return Matrix4([[1.0, 1.0, 1.0, 1.0],
                     [1.0, 1.0, 1.0, 1.0],
                     [1.0, 1.0, 1.0, 1.0]])
 
 
 @pytest.fixture()
-def test_matrix_3():
+def test_matrix4_3():
     return Matrix4([[1.0, 0.0, 0.0, 0.0],
                     [0.0, 1.0, 0.0, 0.0],
                     [0.0, 0.0, 1.0, 0.0],
@@ -25,29 +25,45 @@ def test_matrix_3():
 
 
 @pytest.fixture()
-def test_vector():
+def test_vector3():
     return Vector3(2.0, 2.0, 2.0)
 
 
-def test_make_translation(test_matrix_1, test_vector):
-    test_translation_matrix = test_matrix_1.make_translation(test_vector)
+def test_make_translation(test_matrix4_1, test_vector3):
+    test_translation_matrix = test_matrix4_1.make_translation(test_vector3)
     assert test_translation_matrix == Matrix4([[1.0, 0.0, 0.0, 2.0],
                                                [0.0, 1.0, 0.0, 2.0],
                                                [0.0, 0.0, 1.0, 2.0],
                                                [0.0, 0.0, 0.0, 1.0]])
 
 
-'''
-def test_make_rotation(test_matrix_1):
-    test_rotation_matrix = test_matrix_1.make_translation(0.0)
-    assert test_rotation_matrix == Matrix3([[1.0, 0.0, 0.0],
-                                            [0.0, 1.0, 0.0],
-                                            [0.0, 0.0, 1.0]])
-'''
+def test_make_x_rotation(test_matrix4_1):
+    test_x_rotation_matrix = test_matrix4_1.make_x_rotation(0.0)
+    assert test_x_rotation_matrix == Matrix4([[1.0, 0.0, 0.0, 0.0],
+                                            [0.0, 1.0, 0.0, 0.0],
+                                            [0.0, 0.0, 1.0, 0.0],
+                                            [0.0, 0.0, 0.0, 1.0]])
 
 
-def test_matrix_to_matrix_multiplication(test_matrix_1, test_matrix_2):
-    assert test_matrix_1 * test_matrix_2 == Matrix4([[1.0, 1.0, 1.0, 1.0],
-                                                     [1.0, 1.0, 1.0, 1.0],
-                                                     [1.0, 1.0, 1.0, 1.0],
-                                                     [1.0, 1.0, 1.0, 1.0]])
+def test_make_y_rotation(test_matrix4_1):
+    test_y_rotation_matrix = test_matrix4_1.make_y_rotation(0.0)
+    assert test_y_rotation_matrix == Matrix4([[1.0, 0.0, 0.0, 0.0],
+                                            [0.0, 1.0, 0.0, 0.0],
+                                            [0.0, 0.0, 1.0, 0.0],
+                                            [0.0, 0.0, 0.0, 1.0]])
+
+
+def test_make_z_rotation(test_matrix4_1):
+    test_z_rotation_matrix = test_matrix4_1.make_z_rotation(0.0)
+    assert test_z_rotation_matrix == Matrix4([[1.0, 0.0, 0.0, 0.0],
+                                            [0.0, 1.0, 0.0, 0.0],
+                                            [0.0, 0.0, 1.0, 0.0],
+                                            [0.0, 0.0, 0.0, 1.0]])
+
+
+def test_matrix_to_matrix_multiplication(test_matrix4_1, test_matrix4_2):
+    test_multiplication_matrix = test_matrix4_1 * test_matrix4_2
+    assert test_multiplication_matrix == Matrix4([[1.0, 1.0, 1.0, 1.0],
+                                                  [1.0, 1.0, 1.0, 1.0],
+                                                  [1.0, 1.0, 1.0, 1.0],
+                                                  [1.0, 1.0, 1.0, 1.0]])
