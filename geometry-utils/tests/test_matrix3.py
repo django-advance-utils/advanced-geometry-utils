1213e817f564bbcfc952924eb27ed2840ea1782a
Marvellous Akinyele <marvellous.akinyele@gmail.com>
2021-11-05 12:37:42 +0000

    Create test_matrix3.py

---
+++ geometry-utils/tests/test_matrix3.py
@@ -0,0 +1,42 @@
+import pytest
+
+from two_d.matrix3 import Matrix3
+from two_d.vector2 import Vector2
+
+
+@pytest.fixture
+def test_matrix_1():
+    return Matrix3()
+
+
+@pytest.fixture()
+def test_matrix_2():
+    return Matrix3([[1.0, 1.0, 1.0],
+                    [1.0, 1.0, 1.0],
+                    [1.0, 1.0, 1.0]])
+
+
+@pytest.fixture()
+def test_matrix_3():
+    return Matrix3([[1.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0],
+                    [0.0, 0.0, 1.0]])
+
+
+@pytest.fixture()
+def test_vector():
+    return Vector2(2.0, 2.0)
+
+
+def test_make_translation(test_matrix_1, test_vector):
+    test_translation_matrix = test_matrix_1.make_translation(test_vector)
+    assert test_translation_matrix == Matrix3([[1.0, 0.0, 2.0],
+                                               [0.0, 1.0, 2.0],
+                                               [0.0, 0.0, 1.0]])
+
+
+def test_make_rotation(test_matrix_1):
+    test_rotation_matrix = test_matrix_1.make_translation(0.0)
+    assert test_rotation_matrix == Matrix3([[1.0, 0.0, 0.0],
+                                            [0.0, 1.0, 0.0],
+                                            [0.0, 0.0, 1.0]])
