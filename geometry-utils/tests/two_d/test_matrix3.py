42c0b4fe9c6a96431745609e7d0932559f77b9f0
Marvellous Akinyele <marvellous.akinyele@gmail.com>
2021-11-05 14:12:07 +0000

    Update test_matrix3.py

--- geometry-utils/tests/two_d/test_matrix3.py
+++ geometry-utils/tests/two_d/test_matrix3.py
@@ -5,46 +5,44 @@ from two_d.vector2 import Vector2
 
 
 @pytest.fixture
-def test_matrix_1():
+def test_matrix3_1():
     return Matrix3()
 
 
 @pytest.fixture()
-def test_matrix_2():
+def test_matrix3_2():
     return Matrix3([[1.0, 1.0, 1.0],
                     [1.0, 1.0, 1.0],
                     [1.0, 1.0, 1.0]])
 
 
 @pytest.fixture()
-def test_matrix_3():
+def test_matrix3_3():
     return Matrix3([[1.0, 0.0, 0.0],
                     [0.0, 1.0, 0.0],
                     [0.0, 0.0, 1.0]])
 
 
 @pytest.fixture()
-def test_vector():
+def test_vector2():
     return Vector2(2.0, 2.0)
 
 
-def test_make_translation(test_matrix_1, test_vector):
-    test_translation_matrix = test_matrix_1.make_translation(test_vector)
+def test_make_translation(test_matrix3_1, test_vector2):
+    test_translation_matrix = test_matrix3_1.make_translation(test_vector2)
     assert test_translation_matrix == Matrix3([[1.0, 0.0, 2.0],
                                                [0.0, 1.0, 2.0],
                                                [0.0, 0.0, 1.0]])
 
 
-'''
-def test_make_rotation(test_matrix_1):
-    test_rotation_matrix = test_matrix_1.make_translation(0.0)
+def test_make_rotation(test_matrix3_1):
+    test_rotation_matrix = test_matrix3_1.make_rotation(0.0)
     assert test_rotation_matrix == Matrix3([[1.0, 0.0, 0.0],
                                             [0.0, 1.0, 0.0],
                                             [0.0, 0.0, 1.0]])
-'''
 
-    
-def test_matrix_to_matrix_multiplication(test_matrix_1, test_matrix_2):
-    assert test_matrix_1 * test_matrix_2 == Matrix3([[1.0, 1.0, 1.0],
+
+def test_matrix_to_matrix_multiplication(test_matrix3_1, test_matrix3_2):
+    assert test_matrix3_1 * test_matrix3_2 == Matrix3([[1.0, 1.0, 1.0],
                                                      [1.0, 1.0, 1.0],
                                                      [1.0, 1.0, 1.0]])
