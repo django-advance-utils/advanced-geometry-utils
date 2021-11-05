3f8f2535f1ea6eb7e97118b0125fe9df8368ec1c
Marvellous Akinyele <marvellous.akinyele@gmail.com>
2021-11-05 10:11:31 +0000

    Update Point3.py

--- geometry-utils/three_d/point3.py
+++ geometry-utils/three_d/point3.py
@@ -1,4 +1,4 @@
-from vector3 import Vector3
+from three_d.vector3 import Vector3
 
 
 class Point3:
@@ -10,13 +10,19 @@ class Point3:
 
     def __add__(self, other):
         if isinstance(other, Point3) or isinstance(other, Vector3):
-            return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
+            return Point3(self.x + other.x, self.y + other.y, self.z + other.z)
         else:
             return NotImplemented
 
     def __sub__(self, other):
         if isinstance(other, Point3) or isinstance(other, Vector3):
             return Point3(self.x - other.x, self.y - other.y, self.z - other.z)
+        else:
+            return NotImplemented
+
+    def __mul__(self, other):
+        if isinstance(other, float):
+            return Point3(self.x * other, self.y * other, self.z * other)
         else:
             return NotImplemented
 
@@ -27,3 +33,6 @@ class Point3:
     def __ne__(self, other):
         if isinstance(other, Point3):
             return bool(self.x != other.x or self.y != other.y or self.z != other.z)
+
+    def to_vector(self):
+        return Vector3(self.x, self.y, self.z)
