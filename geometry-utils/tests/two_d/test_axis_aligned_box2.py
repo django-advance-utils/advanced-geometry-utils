59d593e726064f78a12e93943657e8c2263bdd73
Marvellous Akinyele <marvellous.akinyele@gmail.com>
2021-11-05 16:34:44 +0000

    Update test_axis_aligned_box2.py

--- geometry-utils/tests/two_d/test_axis_aligned_box2.py
+++ geometry-utils/tests/two_d/test_axis_aligned_box2.py
@@ -19,5 +19,20 @@ def test_point2_1():
     return Point2(0.0, 0.0)
 
 
-def test_box_contains_point(box2, test_point2_1):
-    assert test_point2_1 in box2
+@pytest.fixture()
+def test_point2_2():
+    return Point2(1.0, 1.0)
+
+
+def test_box_contains_point(box1, test_point2_1, test_point2_2):
+    assert test_point2_1 in box1
+    assert test_point2_2 in box1
+
+
+def test_box_does_not_contain_point(box2, test_point2_2):
+    assert test_point2_2 not in box2
+
+
+def test_box_includes_point(box2, test_point2_2):
+    box2.include(test_point2_2)
+    assert box2.min == Point2(0.0, 0.0) and box2.max == Point2(1.0, 1.0)
