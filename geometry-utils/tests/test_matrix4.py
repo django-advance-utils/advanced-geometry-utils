5d64110f495168ba761e1824e5f79ccbcd1b4d09
Marvellous Akinyele <marvellous.akinyele@gmail.com>
2021-11-05 13:23:46 +0000

    Create test_matrix4.py

---
+++ geometry-utils/tests/test_matrix4.py
@@ -0,0 +1,53 @@
+import pytest
+
+from three_d.matrix4 import Matrix4
+from three_d.vector3 import Vector3
+
+
+@pytest.fixture
+def test_matrix_1():
+    return Matrix4()
+
+
+@pytest.fixture()
+def test_matrix_2():
+    return Matrix4([[1.0, 1.0, 1.0, 1.0],
+                    [1.0, 1.0, 1.0, 1.0],
+                    [1.0, 1.0, 1.0, 1.0]])
+
+
+@pytest.fixture()
+def test_matrix_3():
+    return Matrix4([[1.0, 0.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0, 0.0],
+                    [0.0, 0.0, 1.0, 0.0],
+                    [0.0, 0.0, 0.0, 1.0]])
+
+
+@pytest.fixture()
+def test_vector():
+    return Vector3(2.0, 2.0, 2.0)
+
+
+def test_make_translation(test_matrix_1, test_vector):
+    test_translation_matrix = test_matrix_1.make_translation(test_vector)
+    assert test_translation_matrix == Matrix4([[1.0, 0.0, 0.0, 2.0],
+                                               [0.0, 1.0, 0.0, 2.0],
+                                               [0.0, 0.0, 1.0, 2.0],
+                                               [0.0, 0.0, 0.0, 1.0]])
+
+
+'''
+def test_make_rotation(test_matrix_1):
+    test_rotation_matrix = test_matrix_1.make_translation(0.0)
+    assert test_rotation_matrix == Matrix3([[1.0, 0.0, 0.0],
+                                            [0.0, 1.0, 0.0],
+                                            [0.0, 0.0, 1.0]])
+'''
+
+
+def test_matrix_to_matrix_multiplication(test_matrix_1, test_matrix_2):
+    assert test_matrix_1 * test_matrix_2 == Matrix4([[1.0, 1.0, 1.0, 1.0],
+                                                     [1.0, 1.0, 1.0, 1.0],
+                                                     [1.0, 1.0, 1.0, 1.0],
+                                                     [1.0, 1.0, 1.0, 1.0]])
