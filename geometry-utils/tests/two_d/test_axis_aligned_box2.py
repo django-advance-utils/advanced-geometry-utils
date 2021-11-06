a802c5cf2c1060897cfef45c9c6485b9925cddea
Marvellous Akinyele <marvellous.akinyele@gmail.com>
2021-11-06 13:32:34 +0000

    Update test_axis_aligned_box2.py

--- geometry-utils/tests/two_d/test_axis_aligned_box2.py
+++ geometry-utils/tests/two_d/test_axis_aligned_box2.py
@@ -78,8 +78,8 @@ def test_box_equals_box(box1):
 def test_box_not_equals_box(box1, box2):
     assert box1 != box2
 
-'''
+
 def test_box_is_empty(box4):
     # assert box2.empty()
-    print(box4.max.x)
-'''
+    print(box4.max.y)
+
