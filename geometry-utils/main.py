bf710cf2618fc36d4b658ebfdeedaff4036563f1
Marvellous Akinyele <marvellous.akinyele@gmail.com>
2021-11-08 14:39:57 +0000

    commit

--- geometry-utils/main.py
+++ geometry-utils/main.py
@@ -1,15 +1,21 @@
-from two_d.edge2 import Edge2
-from two_d.path2 import Path2
-from two_d.point2 import Point2
+from three_d.edge3 import Edge3
+from three_d.point3 import Point3
+from three_d.path3 import Path3
 
 if __name__ == '__main__':
 
-    first_edge = Edge2(Point2(0.0, 0.0), Point2(1.0, 1.0))
-    second_edge = Edge2(Point2(1.0, 1.0), Point2(2.0, 2.0))
-    third_edge = Edge2(Point2(2.0, 2.0), Point2(0.0, 0.0))
-
+    first_edge = Edge3(Point3(0.0, 0.0, 0.0), Point3(1.0, 1.0, 1.0))
+    second_edge = Edge3(Point3(1.0, 1.0, 1.0), Point3(2.0, 2.0, 2.0))
+    third_edge = Edge3(Point3(2.0, 2.0, 2.0), Point3(0.0, 0.0, 0.0))
     list_of_edges = [first_edge, second_edge, third_edge]
-    path = Path2(list_of_edges)
+    path = Path3(list_of_edges)
     print(path.is_continuous())
     print(path.is_closed())
+    print(path.get_path_bounds().min.x)
+    print(path.get_path_bounds().min.y)
+    print(path.get_path_bounds().min.z)
+    print(path.get_path_bounds().max.x)
+    print(path.get_path_bounds().max.y)
+    print(path.get_path_bounds().max.z)
+
 
