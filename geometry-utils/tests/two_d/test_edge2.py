f8ac49491b1494f1e6b65b7a81527e6b2e38cda1
Marvellous Akinyele <marvellous.akinyele@gmail.com>
2021-11-08 16:25:05 +0000

    Update test_edge2.py

--- geometry-utils/tests/two_d/test_edge2.py
+++ geometry-utils/tests/two_d/test_edge2.py
@@ -1,5 +1,6 @@
 import pytest
 
+from two_d.axis_aligned_box2 import AxisAlignedBox2
 from two_d.edge2 import Edge2
 from two_d.point2 import Point2
 from two_d.vector2 import Vector2
@@ -10,21 +11,29 @@ from math import sqrt
 
 @pytest.fixture()
 def test_edge2_1():
-    return Edge2
+    return Edge2()
 
 
 @pytest.fixture()
 def test_edge2_2():
     p1 = Point2(0.0, 0.0)
     p2 = Point2(2.0, 2.0)
-    radius = 1.0
 
-    return Edge2(p1, p2, radius)
+    return Edge2(p1, p2)
 
 
-def test_edge_point_parametric(test_edge2_2):
+@pytest.fixture()
+def test_edge2_3():
+    p1 = Point2(2.0, 2.0)
+    p2 = Point2(4.0, 4.0)
+
+    return Edge2(p1, p2)
+
+
+def test_edge_point_parametric(test_edge2_1, test_edge2_2):
     assert test_edge2_2.point_parametric(0.0) == test_edge2_2.p1
     assert test_edge2_2.point_parametric(1.0) == test_edge2_2.p2
+    assert test_edge2_1.point_parametric(0.5) == test_edge2_1.p1  # p1 = p2
 
 
 def test_edge_parametric_point(test_edge2_2):
@@ -34,3 +43,16 @@ def test_edge_parametric_point(test_edge2_2):
 
 def test_get_tangent(test_edge2_2):
     assert test_edge2_2.get_tangent() == Vector2(2.0/sqrt(8.0), 2.0/sqrt(8.0))
+
+
+def test_get_arc_centre(test_edge2_2):
+    assert test_edge2_2.get_arc_centre() == Point2(1.0, 1.0)
+
+
+def test_get_edge_bounds(test_edge2_2):
+    assert test_edge2_2.get_edge_bounds() == AxisAlignedBox2(Point2(0.0, 0.0), Point2(2.0, 2.0))
+
+'''
+def test_edge_intersect(test_edge2_2, test_edge2_3):
+    assert test_edge2_2.intersect(test_edge2_3)
+'''
