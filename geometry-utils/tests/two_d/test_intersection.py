c5cca37abd2508b5938908ad02953b31a876cf39
Marvellous Akinyele <marvellous.akinyele@gmail.com>
2021-11-09 16:06:00 +0000

    Update test_intersection.py

--- geometry-utils/tests/two_d/test_intersection.py
+++ geometry-utils/tests/two_d/test_intersection.py
@@ -10,8 +10,19 @@ def intersection1():
     return Intersection()
 
 
-def test_intersection_lines(intersection1):
+def test_intersection_on_collinear_lines(intersection1):
     intersection1.intersect_lines(Point2(0.0, 0.0), Point2(1.0, 1.0), Point2(2.0, 2.0), Point2(4.0, 4.0))
     assert intersection1.point == Point2(0.0, 0.0)
+    assert intersection1.vectors_intersect
+    assert not intersection1.on_first_segment
+    assert not intersection1.on_second_segment
+    assert not intersection1.end_of_line
+
+
+def test_intersection_on_non_collinear_lines(intersection1):
     intersection1.intersect_lines(Point2(1.0, 4.0), Point2(7.0, 56.0), Point2(14.0, 9.0), Point2(60.0, 55.0))
-    assert floats_are_close(intersection1.point.x, -0.043024771838331)
+    assert floats_are_close(intersection1.point.x, -0.043) and floats_are_close(intersection1.point.y, -5.043)
+    assert intersection1.vectors_intersect
+    assert not intersection1.on_first_segment
+    assert not intersection1.on_second_segment
+    assert not intersection1.end_of_line
