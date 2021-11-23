fbaa8bdc8adac190cd8f051488c1873a0b77a2ac
Marvin Akinyele <marvellous.akinyele@gmail.com>
2021-11-23 08:01:22 +0000

    4 x 4 matrix docstring

--- geometry_utils/three_d/matrix4.py
+++ geometry_utils/three_d/matrix4.py
@@ -1,11 +1,32 @@
 import inspect
 from math import cos, sin
 
-from maths_utility import is_list, is_float
+from maths_utility import is_list, is_int_or_float
 from three_d.vector3 import is_vector3
 
 
 class Matrix4:
+    """
+    A class to create a 4 x 4 matrix
+
+    Attributes:
+    ___________
+    vals:   int or float
+            the elements of the matrix
+
+    Methods:
+    ________
+    set_identity(): Matrix4
+        Returns a 4 x 4 identity matrix
+    __mul__(Matrix4): Matrix4
+        Returns the multiplication of the matrix with another 4 x 4 matrix
+    __eq__(Matrix4): bool
+        Returns the equality comparison of the matrix with another 4 x 4 matrix
+    make_translation(Vector2): Matrix4
+        Creates a 4 x 4 translation matrix
+    make_rotation(int or float): Matrix4
+        Creates a 4 x 4 rotation matrix
+    """
     def __init__(self, vals=None):
         if vals is None:
             self.set_identity()
@@ -18,9 +39,24 @@ class Matrix4:
                 raise AttributeError("Input Matrix must be 4 x 4")
 
     def set_identity(self):
+        """
+        Converts the matrix to an identity matrix
+
+        :return:the identity matrix
+        :rtype: Matrix4
+        """
         self.vals = [[1.0 if i == j else 0.0 for i in range(4)] for j in range(4)]
 
     def __mul__(self, other):
+        """
+        Calculates the matrix multiplication of self with another 4 x 4 matrix
+
+        :param  other: the right hand side matrix
+        :type   other: Matrix4
+        :return:the resulting multiplied matrix
+        :rtype: Matrix4
+        :raises:TypeError: wrong argument type
+        """
         if is_matrix4(other):
             result = Matrix4()
             for i in range(4):
@@ -31,12 +67,30 @@ class Matrix4:
         raise TypeError("Multiplication must be done with an object of Matrix4")
 
     def __eq__(self, other):
+        """
+        Compares the equality of self and other 4 x 4 matrix.
+
+        :param  other: the other matrix
+        :type   other: Matrix4
+        :return:the point equality
+        :rtype: bool
+        :raises:TypeError: Wrong argument type
+        """
         if is_matrix4(other) or inspect.isclass(other):  # revisit
             return [[True if i == j else False for i in self.vals] for j in other.vals]
         raise TypeError("Comparison must be with another object of Matrix4")
 
     @classmethod
     def make_translation(cls, vector):
+        """
+        Creates a translation matrix using the 3D vector
+
+        :param  vector: the translation vector
+        :type   vector: Vector3
+        :return:translation matrix
+        :rtype: Matrix4
+        :raises:TypeError: Wrong argument type
+        """
         if is_vector3(vector):
             mat = cls
             mat.vals = [[1.0, 0.0, 0.0, vector.x],
@@ -49,7 +103,16 @@ class Matrix4:
 
     @classmethod
     def make_x_rotation(cls, theta):
-        if is_float(theta):
+        """
+        Creates an x-axis rotation matrix using an angle
+
+        :param  theta: the angle of rotation
+        :type   theta: int/float
+        :return:x-axis rotation matrix
+        :rtype: Matrix4
+        :raises:TypeError: Wrong argument type
+        """
+        if is_int_or_float(theta):
             mat = cls
             cos_theta = cos(theta)
             sin_theta = sin(theta)
@@ -59,11 +122,20 @@ class Matrix4:
                         [0.0,  0.0,       0.0,       1.0]]
 
             return mat
-        raise TypeError("X rotation must be with a float")
+        raise TypeError("X rotation must be with an int or float")
 
     @classmethod
     def make_y_rotation(cls, theta):
-        if is_float(theta):
+        """
+        Creates a y-axis rotation matrix using an angle
+
+        :param  theta: the angle of rotation
+        :type   theta: int/float
+        :return:y-axis rotation matrix
+        :rtype: Matrix4
+        :raises:TypeError: Wrong argument type
+        """
+        if is_int_or_float(theta):
             mat = cls
             cos_theta = cos(theta)
             sin_theta = sin(theta)
@@ -73,11 +145,20 @@ class Matrix4:
                         [0.0,       0.0,  0.0,       1.0]]
 
             return mat
-        raise TypeError("Y rotation must be with a float")
+        raise TypeError("Y rotation must be with an int or float")
 
     @classmethod
     def make_z_rotation(cls, theta):
-        if is_float(theta):
+        """
+        Creates a z-axis rotation matrix using an angle
+
+        :param  theta: the angle of rotation
+        :type   theta: int/float
+        :return:z-axis rotation matrix
+        :rtype: Matrix4
+        :raises:TypeError: Wrong argument type
+        """
+        if is_int_or_float(theta):
             mat = cls
             cos_theta = cos(theta)
             sin_theta = sin(theta)
@@ -87,7 +168,7 @@ class Matrix4:
                         [0.0,        0.0,       0.0, 1.0]]
 
             return mat
-        raise TypeError("Z rotation must be with a float")
+        raise TypeError("Z rotation must be with an int or float")
 
 
 def is_matrix4(input_variable):
