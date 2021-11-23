00e4d31a6b65ef004f676fd111684ffebee208d5
Marvin Akinyele <marvellous.akinyele@gmail.com>
2021-11-23 08:03:49 +0000

    2D path docstring

--- geometry_utils/two_d/path2.py
+++ geometry_utils/two_d/path2.py
@@ -4,6 +4,28 @@ from two_d.edge2 import is_edge2
 
 
 class Path2:
+    """
+    A class to create a 2D path
+
+    Attributes:
+    ___________
+    list_of_edges: list
+        the list of 2D edges to establish a path
+    first_edge: Edge2
+        the first 2D edge on the path
+    last_edge: Edge2
+        the last 2D edge on the path
+
+    Methods:
+    ________
+    is_closed(): bool
+        Returns the result of the test if the path is closed
+    is_continuous(): bool
+        Returns the result of the test if the path is continuous
+    get_path_bounds(): AxisAlignedBox2()
+        Returns 2D box containing the edges of the path
+    """
+
     def __init__(self, list_of_edges):
         if is_list(list_of_edges) and all(is_edge2(edge) for edge in list_of_edges):
             self.list_of_edges = list_of_edges
@@ -13,9 +35,21 @@ class Path2:
             raise TypeError("Path2 argument must be a list of objects of Edge2")
 
     def is_closed(self):
+        """
+        Tests if the path is closed
+
+        :return:closeness of the path
+        :rtype: bool
+        """
         return self.last_edge.p2 == self.first_edge.p1 and self.is_continuous()
 
     def is_continuous(self):
+        """
+        Tests if the path is continuous
+
+        :return:continuity of the path
+        :rtype: bool
+        """
         continuity = True
         for edge, next_edge in zip(self.list_of_edges, self.list_of_edges[1:]):
             if edge.p2 != next_edge.p1:
@@ -23,6 +57,12 @@ class Path2:
         return continuity
 
     def get_path_bounds(self):
+        """
+        Derives the AxisAlignedBox2 containing the bounds of the path
+
+        :return:the box containing the path bounds
+        :rtype: AxisAlignedBox2
+        """
         path_bounds = AxisAlignedBox2()
         for edge in self.list_of_edges:
             path_bounds.include(edge.get_edge_bounds())
