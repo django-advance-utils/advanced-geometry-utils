8101be2afb2bdf1a344a4de63644b4231437daef
Marvin Akinyele <marvellous.akinyele@gmail.com>
2021-12-01 16:03:48 +0000

    Update ellipse.py

--- geometry_utils/two_d/ellipse.py
+++ geometry_utils/two_d/ellipse.py
@@ -43,22 +43,18 @@ class Ellipse:
 
     def __init__(self,
                  start=Point2(0.0, 0.0),
-                 centre=Point2(0.0, 0.0),
+                 centre=None,
                  end=Point2(0.0, 0.0),
                  major_radius=0.0,
                  minor_radius=0.0,
                  clockwise=False,
                  large_arc=False,
-                 angle=0.0):
+                 angle=None):
 
-        if (is_point2(start) and is_point2(centre) and is_point2(end) and
+        if (is_point2(start) and is_point2(end) and
                 are_ints_or_floats([major_radius, minor_radius])):
 
             self.start = start
-            if centre:
-                self.centre = centre
-            else:
-                self.centre = self.calculate_centre()
             self.end = end
             self.major_radius = major_radius
             self.minor_radius = minor_radius
@@ -67,6 +63,10 @@ class Ellipse:
             self.angle = angle
             self.delta = 0.0
             self.valid = self.test_validity()
+            if centre is not None and is_point2(centre):
+                self.centre = centre
+            else:
+                self.centre = self.calculate_centre()
 
         else:
             if not is_point2(start) or not is_point2(centre) or not is_point2(end):
@@ -82,7 +82,6 @@ class Ellipse:
         angle_in_radians = degrees_to_radians(self.angle)
         sin_phi = sin(angle_in_radians)
         cos_phi = cos(angle_in_radians)
-
         x_dash = (cos_phi * ((self.start.x - self.end.x) / 2.0)) + (sin_phi * ((self.start.y - self.end.y) / 2.0))
         y_dash = (-sin_phi * ((self.start.x - self.end.x) / 2.0)) + (cos_phi * ((self.start.y - self.end.y) / 2.0))
 
@@ -95,13 +94,14 @@ class Ellipse:
             root_delta = sqrt(self.delta)
             rx *= root_delta
             ry *= root_delta
+            self.valid = False
 
         numerator = ((rx * rx) * (ry * ry)) - ((rx * rx) * (y_dash * y_dash)) - ((ry * ry) * (x_dash * x_dash))
         denominator = ((rx * rx) * (y_dash * y_dash)) + ((ry * ry) * (x_dash * x_dash))
 
-        root_part = 0.0
-        if numerator > 0.0:
-            root_part = sqrt(numerator / denominator)
+        root_part = sqrt(numerator / denominator)
+        if numerator < 0.0:
+            root_part = 0.0
 
         if self.large_arc != self.clockwise:
             root_part *= -1
