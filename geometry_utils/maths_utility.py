18eb047d0361376185b4cfa3717a8768438ba6cd
Marvin Akinyele <marvellous.akinyele@gmail.com>
2021-12-03 12:22:14 +0000

    rename global variables

--- geometry_utils/maths_utility.py
+++ geometry_utils/maths_utility.py
@@ -1,72 +1,13 @@
-def PI():
-    return 3.14159265358
+PI = float(3.141592653589793238)
 
+HALF_PI = float(PI * (1/2))
+ONE_AND_HALF_PI = float(PI * (3/2))
+TWO_PI = float(PI * 2)
 
-def double_pi():
-    return PI() * 2
-
-
-def double_epsilon():
-    return 0.0001
-
-
-def deg_to_rad():
-    return PI() / 180.0
-
-
-def degrees_to_radians(theta_in_degrees):
-    if isinstance(theta_in_degrees, float):
-        return theta_in_degrees * deg_to_rad()
-
-
-def is_float(input_variable):
-    return isinstance(input_variable, float)
-
-
-def is_int(input_variable):
-    return isinstance(input_variable, int)
-
-
-def is_list(input_variable):
-    return isinstance(input_variable, list)
-
-
-def is_int_or_float(input_variable):
-    return is_float(input_variable) or is_int(input_variable)
-
-
-def are_ints_or_floats(input_list):
-    if isinstance(input_list, list):
-        for input_variable in input_list:
-            return is_int_or_float(input_variable)
-    else:
-        raise TypeError("Input argument must be a list")
-
-
-def floats_are_close(a, b, rel_tol=1e-9, abs_tol=double_epsilon()):
-    return abs(a - b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
-
-
-def is_in_range(value, minimum_value_in_range, maximum_value_in_range):
-    return (value >= (minimum_value_in_range - double_epsilon())) and \
-           (value <= (maximum_value_in_range + double_epsilon()))
-
-
-def ranges_overlap(range1_minimum, range1_maximum, range2_minimum, range2_maximum):
-    return (is_in_range(range1_minimum, range2_minimum, range2_maximum)) or \
-           (is_in_range(range1_maximum, range2_minimum, range2_maximum)) or \
-           (is_in_range(range2_minimum, range1_minimum, range1_maximum)) or \
-           (is_in_range(range2_maximum, range1_minimum, range1_maximum))
-
-
-def change_coordinates(angle):
-    new_angle = angle - (1.5 * PI())
-    return new_angle
-
+DEG_TO_RAD = PI / 180.0
 
 CIRCLE_DIVISIONS = 512
 
-# circle factors are y, x not x, y
 CIRCLE_FACTORS = [[1.000000, 0.000000],
                   [0.999925, 0.012272],
                   [0.999699, 0.024541],
@@ -579,3 +520,50 @@ CIRCLE_FACTORS = [[1.000000, 0.000000],
                   [0.999322, -0.036807],
                   [0.999699, -0.024541],
                   [0.999925, -0.012272]]
+
+DOUBLE_EPSILON = 0.0001
+
+
+def degrees_to_radians(theta_in_degrees):
+    if isinstance(theta_in_degrees, float):
+        return theta_in_degrees * DEG_TO_RAD
+
+
+def is_float(input_variable):
+    return isinstance(input_variable, float)
+
+
+def is_int(input_variable):
+    return isinstance(input_variable, int)
+
+
+def is_list(input_variable):
+    return isinstance(input_variable, list)
+
+
+def is_int_or_float(input_variable):
+    return is_float(input_variable) or is_int(input_variable)
+
+
+def are_ints_or_floats(input_list):
+    if isinstance(input_list, list):
+        for input_variable in input_list:
+            return is_int_or_float(input_variable)
+    else:
+        raise TypeError("Input argument must be a list")
+
+
+def floats_are_close(a, b, rel_tol=1e-9, abs_tol=DOUBLE_EPSILON):
+    return abs(a - b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+
+
+def is_in_range(value, minimum_value_in_range, maximum_value_in_range):
+    return (value >= (minimum_value_in_range - DOUBLE_EPSILON)) and \
+           (value <= (maximum_value_in_range + DOUBLE_EPSILON))
+
+
+def ranges_overlap(range1_minimum, range1_maximum, range2_minimum, range2_maximum):
+    return (is_in_range(range1_minimum, range2_minimum, range2_maximum)) or \
+           (is_in_range(range1_maximum, range2_minimum, range2_maximum)) or \
+           (is_in_range(range2_minimum, range1_minimum, range1_maximum)) or \
+           (is_in_range(range2_maximum, range1_minimum, range1_maximum))
