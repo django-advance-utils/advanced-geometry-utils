ad10da2d4bfc0597fb1c9aeb440fcbf80069b7ca
Marvin Akinyele <marvellous.akinyele@gmail.com>
2021-12-08 15:49:18 +0000

    update path2.py

--- geometry_utils/two_d/path2.py
+++ geometry_utils/two_d/path2.py
@@ -1,9 +1,8 @@
-from copy import deepcopy
+from copy import deepcopy, copy
 
-from geometry_utils.maths_utility import degrees_to_radians, is_int_or_float
+from geometry_utils.maths_utility import is_int_or_float
 from geometry_utils.two_d.axis_aligned_box2 import AxisAlignedBox2
 from geometry_utils.two_d.edge2 import Edge2
-from geometry_utils.two_d.matrix3 import Matrix3
 from geometry_utils.two_d.vector2 import is_vector2
 
 
@@ -35,7 +34,12 @@ class Path2:
 
     def __eq__(self, other_path):
         if is_path2(other_path):
-            return self.list_of_edges == other_path.list_of_edges
+            if self.path_length != other_path.path_length:
+                return False
+            for index, enum in enumerate(self.list_of_edges):
+                if self.list_of_edges[index] != other_path.list_of_edges[index]:
+                    return False
+            return True
         raise TypeError("Comparison must be done with another object of Path2")
 
     @property
@@ -104,7 +108,10 @@ class Path2:
         return path_bounds
 
     def to_tuple_list(self):
-        return [(edge.p1, edge.p2) for edge in self.list_of_edges]
+        path_tuple_list = []
+        for edge in self.list_of_edges:
+            path_tuple_list.append((edge.p1, edge.p2))
+        return path_tuple_list
 
     def remove_duplicate_edges(self):
         indices_of_edges_to_remove = []
@@ -135,6 +142,7 @@ class Path2:
         if is_vector2(vector):
             for edge in self.list_of_edges:
                 edge.offset_edge(vector)
+            return self
         else:
             raise TypeError("Path offset must be done with a vector")
 
@@ -144,31 +152,28 @@ class Path2:
             self.offset_path(reversed_rotation_vector)
             self.rotate(rotation_angle)
             self.offset_path(rotation_vector)
+        return self
 
     def rotate(self, rotation_angle):
-        rotation_angle_in_radians = degrees_to_radians(rotation_angle)
-
-        rotation_matrix = Matrix3()
-        rotation_matrix.make_rotation(rotation_angle_in_radians)
-
         for edge in self.list_of_edges:
-            edge.p1 = rotation_matrix * edge.p1
-            edge.p2 = rotation_matrix * edge.p2
+            edge.rotate(rotation_angle)
+        return self
 
     def close_path(self):
         if self.path_length > 1 and not self.is_closed:
-            self.list_of_edges.append(Edge2(self.list_of_edges[-1].p2, self.list_of_edges[0].p1))
+            self.list_of_edges.append(Edge2(deepcopy(self.list_of_edges[-1].p2), deepcopy(self.list_of_edges[0].p1)))
+        return self
 
     def is_circle(self):
         return self.path_length == 1 and self.list_of_edges[0].is_circle()
 
     def get_enclosed_area(self):
-        if not self.is_closed or self.path_length == 0:
-            raise TypeError("The path must be closed and have more than one edge")
-
         path = deepcopy(self)
 
         path.remove_duplicate_edges()
+        if path.is_closed and path.path_length != 0:
+            return path
+        raise TypeError("The path must be closed and have more than one edge")
 
     # to be reviewed with Simon and Tom
     def remove_arcs(self):
@@ -185,7 +190,8 @@ class Path2:
         index_offset = 0
         for new_edge in list_of_edges_to_remove:
             offset_location = new_edge[0] + index_offset
-            self.list_of_edges[offset_location] = new_edge[1]
+            del self.list_of_edges[offset_location]
+            self.list_of_edges[offset_location:offset_location] = new_edge[1]
             index_offset += len(new_edge[1]) - 1
 
     def is_quadrilateral(self):
