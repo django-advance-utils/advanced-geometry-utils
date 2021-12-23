a218490324e14cd24c9d617933c0508f1d78abb9
Marvin Akinyele <marvellous.akinyele@gmail.com>
2021-12-23 17:37:16 +0000

    Update path3.py

--- geometry_utils/three_d/path3.py
+++ geometry_utils/three_d/path3.py
@@ -1,6 +1,4 @@
-from geometry_utils.maths_utility import is_list
 from geometry_utils.three_d.axis_aligned_box3 import AxisAlignedBox3
-from geometry_utils.three_d.edge3 import is_edge3
 
 
 class Path3:
@@ -25,13 +23,52 @@ class Path3:
     get_path_bounds(): AxisAlignedBox3()
         Returns 3D box containing the edges of the path
     """
-    def __init__(self, list_of_edges):
-        if is_list(list_of_edges) and all(is_edge3(edge) for edge in list_of_edges):
-            self.list_of_edges = list_of_edges
-            self.first_edge = self.list_of_edges[0]
-            self.last_edge = self.list_of_edges[-1]
+    def __init__(self):
+        self.list_of_edges = []
+
+    def print_edges(self):
+        print "Path3(list of edges: "
+        for index, edge in enumerate(self.list_of_edges):
+            print ("\t" + str(index) + "\t" + str(edge))
+        print ")"
+
+    def __str__(self):
+        self.print_edges()
+        return ""
+
+    def __eq__(self, other_path):
+        if is_path3(other_path) and self.path_length == other_path.path_length:
+            for index in range(self.path_length):
+                if self.list_of_edges[index] != other_path.list_of_edges[index]:
+                    return False
+            return True
         else:
-            raise TypeError("Path3 argument must be a list of objects of Edge3")
+            if not is_path3(other_path):
+                raise TypeError("Comparison must be done with another object of Path2")
+            if self.path_length != other_path.path_length:
+                raise IndexError("Comparison must be done with another path of equal number of edges")
+
+    @property
+    def get_first_edge(self):
+        if self.path_length >= 1:
+            return self.list_of_edges[0]
+        raise IndexError("Can not find the first edge of an empty list of edges")
+
+    @property
+    def get_last_edge(self):
+        if self.path_length >= 1:
+            return self.list_of_edges[-1]
+        raise IndexError("Can not find the last edge of an empty list of edges")
+
+    @property
+    def path_length(self):
+        """
+        Calculates the number of Edge3 edges in the path
+
+        :return: number of edges in the path
+        :rtype: int
+        """
+        return len(self.list_of_edges)
 
     def is_closed(self):
         """
@@ -40,7 +77,7 @@ class Path3:
         :return:closeness of the path
         :rtype: bool
         """
-        return self.last_edge.p2 == self.first_edge.p1 and self.is_continuous()
+        return self.list_of_edges[-1].p2 == self.list_of_edges[0].p1 and self.is_continuous()
 
     def is_continuous(self):
         """
@@ -50,12 +87,15 @@ class Path3:
         :rtype: bool
         """
         continuity = True
-        for edge, next_edge in zip(self.list_of_edges, self.list_of_edges[1:]):
-            if edge.p2 != next_edge.p1:
-                continuity = False
+        if self.path_length < 2:
+            continuity = False
+        else:
+            for edge, next_edge in zip(self.list_of_edges, self.list_of_edges[1:]):
+                if edge.p2 != next_edge.p1:
+                    continuity = False
         return continuity
 
-    def get_path_bounds(self):
+    def get_bounds(self):
         """
         Derives the AxisAlignedBox3 containing the bounds of the path
 
@@ -67,3 +107,6 @@ class Path3:
             path_bounds.include(edge.get_edge_bounds())
         return path_bounds
 
+
+def is_path3(input_variable):
+    return isinstance(input_variable, Path3)
