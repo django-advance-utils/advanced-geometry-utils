fd4bbeda0d35ecd227c9210289a3bcf150f373b5
Marvin Akinyele <marvellous.akinyele@gmail.com>
2022-01-10 16:46:20 +0000

    update tests

--- geometry_utils/pytests/two_d/test_axis_aligned_box2.py
+++ geometry_utils/pytests/two_d/test_axis_aligned_box2.py
@@ -5,6 +5,15 @@ from geometry_utils.two_d.point2 import Point2
 from geometry_utils.two_d.vector2 import Vector2
 
 
+def test_box2_with_string_inputs():
+    with pytest.raises(TypeError):
+        return AxisAlignedBox2("0", "0")
+
+
+def test_box2_print_string(test_box2_2):
+    assert test_box2_2.__str__() == "AxisAlignedBox2(min:Point2(x:0.00, y:0.00), max:Point2(x:0.00, y:0.00))"
+
+
 def test_box2_box2_addition_return_arithmetic(test_box2_1, test_box2_2):
     with pytest.raises(TypeError):
         return test_box2_1 + test_box2_2
@@ -42,9 +51,19 @@ def test_box2_does_not_contain_box2(test_box2_1, test_box2_3):
     assert test_box2_1 not in test_box2_3
 
 
-def test_box2_includes_box2(test_box2_1, test_box2_3):
+def test_box2_contain_float(test_box2_1):
+    with pytest.raises(TypeError):
+        return 9.0 in test_box2_1
+
+
+def test_box2_include_box2(test_box2_1, test_box2_3):
     test_box2_3.include(test_box2_1)
     assert test_box2_3.min == Point2(0.0, 0.0) and test_box2_3.max == Point2(2.0, 2.0)
+
+
+def test_box2_include_float(test_box2_1):
+    with pytest.raises(TypeError):
+        return test_box2_1.include(9.0)
 
 
 def test_box2_intersects_box2(test_box2_1, test_box2_2, test_box2_3):
