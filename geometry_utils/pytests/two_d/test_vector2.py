a2b41aa7e9d191bfb40f2ea2d6627c66c35486c8
Marvin Akinyele <marvellous.akinyele@gmail.com>
2022-03-23 12:31:58 +0000

    Update str test

--- geometry_utils/pytests/two_d/test_vector2.py
+++ geometry_utils/pytests/two_d/test_vector2.py
@@ -13,7 +13,7 @@ def test_vector2_string_parameter():
 def test_edge2_print_string(test_edge2_1):
     assert (test_edge2_1.__str__() ==
             "Edge2(p1:Point2(x:0.00, y:0.00), p2:Point2(x:0.00, y:0.00), "
-            "centre:Point2(x:0.00, y:0.00), radius: 0, clockwise:False, large:False)")
+            "centre:Point2(x:0.00, y:0.00), radius:0.0, clockwise:False, large:False)")
 
 
 '''
