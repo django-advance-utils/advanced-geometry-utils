967117e98892517d498d91709b7ef292155b35be
Marvin Akinyele <marvellous.akinyele@gmail.com>
2022-04-01 12:17:17 +0100

    update pathfield interpreter tests

--- geometry_utils/pytests/test_path_field_interpreter.py
+++ geometry_utils/pytests/test_path_field_interpreter.py
@@ -3,6 +3,37 @@ import pytest
 from geometry_utils.path_field_interpreter import PathFieldInterpreter
 
 
-def test_path_field(path2_1):
+def test_path_field_add_field_closed_line(path2_1):
     test_path_field_interpreter = PathFieldInterpreter()
-    test_path_field_interpreter.add_path(path2_1)
+    path_str = test_path_field_interpreter.add_path(path2_1)
+    assert path_str == ';1:1;2:2;#'
+
+
+def test_path_field_add_field_curved_head(path2_8):
+    test_path_field_interpreter = PathFieldInterpreter()
+    path_str = test_path_field_interpreter.add_path(path2_8)
+    assert path_str == ';1;:1;0)0.5;#'
+
+
+def test_path_field_add_field_circle(path2_6):
+    test_path_field_interpreter = PathFieldInterpreter()
+    path_str = test_path_field_interpreter.add_path(path2_6)
+    assert path_str == '1:1)1'
+
+
+def test_path_field_load_path_closed_line(path2_1):
+    test_path_field_interpreter = PathFieldInterpreter()
+    path = test_path_field_interpreter.load_path(';1:1;2:2;#')
+    assert path == path2_1
+
+
+def test_path_field_load_path_curved_head(path2_8):
+    test_path_field_interpreter = PathFieldInterpreter()
+    path = test_path_field_interpreter.load_path(';1;:1;0)0.5;#')
+    assert path == path2_8
+
+
+def test_path_field_load_path_circle(path2_6):
+    test_path_field_interpreter = PathFieldInterpreter()
+    path = test_path_field_interpreter.load_path('1:1)1')
+    assert path == path2_6
